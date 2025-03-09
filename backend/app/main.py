41f5c0f471068b7d8fd84387c09848cdc735efbe
17sTomy <tomasignacioalv@hotmail.com>
2025-03-08 21:12:56 -0300

    Add root endpoint to return API status message

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,3 +4,7 @@ from app.routes.websockets_routes import router as websocket_router
 app = FastAPI()
 
 app.include_router(websocket_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "API is running"}
